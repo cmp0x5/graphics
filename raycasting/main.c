f047c7e78b6cd4edd4a96e26f66dbdc6a82da6a8
Victor <camposvictor035@gmail.com>
2025-03-05 14:53:25 -0300

    check

--- raycasting/main.c
+++ raycasting/main.c
@@ -69,6 +69,107 @@ void drawPlayer()
     glEnd();
 }
 
+void drawRays3D()
+{
+    int r, mx, my, mp, dof;
+    float rayX, rayY, rayAngle, xOffset, yOffset;
+    rayAngle = playerAngle;
+    for (r = 0; r < 1; r++)
+    {
+        /* check horizontal lines */
+        dof = 0;
+        float atan = -1 / tan(rayAngle);
+        if (rayAngle > PI)
+        {
+            rayY = (((int)playerY >> 6) << 6) - 0.0001; // round rayY to nearest 64th value
+            rayX = (playerY - rayY) * atan + playerX;
+            yOffset = -64;
+            xOffset = -yOffset * atan;
+        }
+        if (rayAngle < PI)
+        {
+            rayY = (((int)playerY >> 6) << 6) + 64;
+            rayX = (playerY - rayY) * atan + playerX;
+            yOffset = 64;
+            xOffset = -yOffset * atan;
+        }
+        if (rayAngle == 0 || rayAngle == PI) // looking straight left or right
+        {
+            rayX = playerX;
+            rayY = playerY;
+            dof = 8;
+        }
+        while (dof < 8)
+        {
+            mx = (int) (rayX) >> 6;
+            my = (int) (rayY) >> 6;
+            mp = my * mapX + mx;
+            if (mp < mapX * mapY && map[mp] == 1) // hit wall
+            {
+                dof = 8;
+            }
+            else // next line
+            {
+                rayX += xOffset;
+                rayY += yOffset;
+                dof += 1;
+            }
+        }
+        glColor3f(0, 1, 0);
+        glLineWidth(1);
+        glBegin(GL_LINES);
+        glVertex2i(playerX, playerY);
+        glVertex2i(rayX, rayY);
+        glEnd();
+
+        /* check vertical lines */
+        dof = 0;
+        atan = -tan(rayAngle);
+        if (rayAngle > (PI/2) && rayAngle < (3*PI/2))
+        {
+            rayX = (((int)playerX >> 6) << 6) - 0.0001; 
+            rayY = (playerX - rayX) * atan + playerY;
+            xOffset = -64;
+            yOffset = -xOffset * atan;
+        }
+        if (rayAngle < (PI/2) || rayAngle > (3*PI/2))
+        {
+            rayX = (((int)playerX >> 6) << 6) + 64;
+            rayY = (playerX - rayX) * atan + playerY;
+            xOffset = 64;
+            yOffset = -xOffset * atan;
+        }
+        if (rayAngle == 0 || rayAngle == PI) // looking straight left or right
+        {
+            rayX = playerX;
+            rayY = playerY;
+            dof = 8;
+        }
+        while (dof < 8)
+        {
+            mx = (int) (rayX) >> 6;
+            my = (int) (rayY) >> 6;
+            mp = my * mapX + mx;
+            if (mp < mapX * mapY && map[mp] == 1) // hit wall
+            {
+                dof = 8;
+            }
+            else // next line
+            {
+                rayX += xOffset;
+                rayY += yOffset;
+                dof += 1;
+            }
+        }
+        glColor3f(1, 0, 0);
+        glLineWidth(1);
+        glBegin(GL_LINES);
+        glVertex2i(playerX, playerY);
+        glVertex2i(rayX, rayY);
+        glEnd();
+    }
+}
+
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
@@ -81,20 +182,20 @@ void buttons(unsigned char key, int x, int y)
 {
     if (key == 'a')
     {
-        playerAngle -= 0.1;
-        if (playerAngle < 0)
+        playerAngle += 0.1;
+        if (playerAngle > 2 * PI)
         {
-            playerAngle += 2 * PI;
+            playerAngle -= 2 * PI;
         }
         playerDeltaX = cos(playerAngle) * 5;
         playerDeltaY = sin(playerAngle) * 5;
     }
     if (key == 'd')
     {
-        playerAngle += 0.1;
-        if (playerAngle > 2 * PI)
+        playerAngle -= 0.1;
+        if (playerAngle < 0)
         {
-            playerAngle -= 2 * PI;
+            playerAngle += 2 * PI;
         }
         playerDeltaX = cos(playerAngle) * 5;
         playerDeltaY = sin(playerAngle) * 5;
