78d548399ceddd91fb01eea5507225c725bcf52b
cmp0x5 <camposvictor035@gmail.com>
2026-01-17 21:39:39 -0300

    struct

--- 3d/script.js
+++ 3d/script.js
@@ -14,12 +14,20 @@ function clear()
     ctx.fillRect(0, 0, game.width, game.height)
 }
 
-function point(x, y)
+function point({x, y})
 {
     const s = 20;
     ctx.fillStyle = FOREGROUND
-    ctx.fillRect(x, y, s, s)
+    ctx.fillRect(x - s/2, y - s/2, s, s)
+}
+
+function screen(p)
+{
+    return {
+        x: (p.x + 1)/2*game.width,
+        y: (p.y + 1)/2*game.height,
+    }
 }
 
 clear()
-point(100, 100)
+point(screen({x: 0, y: 0}))
